fb358932ce65a08bd0b397e8d723a81a8f16ec27
Jeff Turner <jeff.turner@gofmx.com>
2025-09-27 17:51:44 -0400

    added some additonal tests for the valid properties of the build response

--- ADOBuildLight.Tests/BuildResponseTests.cs
+++ ADOBuildLight.Tests/BuildResponseTests.cs
@@ -6,21 +6,53 @@ namespace ADOBuildLight.Tests
     [TestFixture]
     public class BuildResponseTests
     {
-        [Test]
-        public void BuildResponse_Properties_CanBeSet()
+        [TestCase("none")]
+        [TestCase("succeeded")]
+        [TestCase("partiallySucceeded")]
+        [TestCase("failed")]
+        [TestCase("canceled")]
+        public void BuildResponse_Result_CanBeSet(string result)
         {
             // Arrange
             var buildResponse = new BuildResponse();
-            var status = "completed";
-            var result = "succeeded";
 
             // Act
-            buildResponse.Status = status;
             buildResponse.Result = result;
 
             // Assert
-            Assert.That(buildResponse.Status, Is.EqualTo(status));
             Assert.That(buildResponse.Result, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void BuildResponse_Result_CanBeSetToNull()
+        {
+            // Arrange
+            var buildResponse = new BuildResponse();
+
+            // Act
+            buildResponse.Result = null;
+
+            // Assert
+            Assert.That(buildResponse.Result, Is.Null);
+        }
+
+        [TestCase("none")]
+        [TestCase("inProgress")]
+        [TestCase("completed")]
+        [TestCase("canceling")]
+        [TestCase("postponed")]
+        [TestCase("notStarted")]
+        [TestCase("all")]
+        public void BuildResponse_Status_CanBeSet(string status)
+        {
+            // Arrange
+            var buildResponse = new BuildResponse();
+
+            // Act
+            buildResponse.Status = status;
+
+            // Assert
+            Assert.That(buildResponse.Status, Is.EqualTo(status));
         }
     }
 }
